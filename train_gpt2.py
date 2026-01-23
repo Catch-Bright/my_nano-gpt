d2728b0fbea064267cbd98d5471710755fd5fcc1
tsinghua-wyh <1642951595@qq.com>
2026-01-23 05:14:22 -0500

    梯度裁剪和超参调度

--- train_gpt2.py
+++ train_gpt2.py
@@ -237,7 +237,7 @@ model.to(device)
 model=torch.compile(model)
 
 #optimize!
-optimizer=torch.optim.AdamW(model.parameters(), lr=3e-4)
+optimizer=torch.optim.AdamW(model.parameters(), lr=3e-4,betas=(0.9,0.95),eps=1e-8)
 for i in range(50):
     t0=time.time()
     x,y=train_loader.next_batch()
@@ -246,12 +246,13 @@ for i in range(50):
     with torch.autocast(device_type=device, dtype=torch.bfloat16):
         logits,loss=model(x,y)
     loss.backward()
+    norm=torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
     optimizer.step()
     torch.cuda.synchronize()
     t1=time.time()
     dt=(t1-t0)*1000
     token_per_sec=(train_loader.B*train_loader.T)/(t1-t0)
-    print(f"step {i}, loss {loss.item()}, dt: {dt:.2f}ms, {token_per_sec:.2f} tokens/sec")
+    print(f"step {i}, loss {loss.item()}, norm:{norm:.4f}, dt: {dt:.2f}ms, {token_per_sec:.2f} tokens/sec")
 
 # logits,loss=model(x,y)
 # print(loss)  #(B, T, vocab_size)
